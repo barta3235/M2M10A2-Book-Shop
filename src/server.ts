ccb8a16fcd3587c18e34885de7e6d39aaa1238d8
ahnaf daiyan <bartamitdaiyan@gmail.com>
2025-01-26 22:52:09 +0600

    eslint and prettier setup

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import app from './app'
-import config from '../src/app/modules/config/index'
-import mongoose from 'mongoose'
-
-
+import app from './app';
+import config from '../src/app/modules/config/index';
+import mongoose from 'mongoose';
 
 async function Bootstrap() {
-    await mongoose.connect(`mongodb+srv://${config.db_name}:${config.db_password}@cluster0.bnhb0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-    app.listen(config.port, () => {
-        console.log(`Example app listening on port ${config.port}`)
-      })
+  await mongoose.connect(
+    `mongodb+srv://${config.db_name}:${config.db_password}@cluster0.bnhb0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+  );
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`);
+  });
 }
 
-Bootstrap()
+Bootstrap();
