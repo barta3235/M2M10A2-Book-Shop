98f56a96e78b82c5de3b02625dac83f7c342ac4a
ahnaf daiyan <bartamitdaiyan@gmail.com>
2025-02-01 20:48:36 +0600

    interface and model created ---product-module

---
+++ src/app/modules/Product/product.model.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productSchema= new Schema<TProduct>({
+    title:{type:String,required:[true,'Book title is required']},
+    author:{type:String,required:[true,'Book author is required']},
+    price:{type:Number,required:[true,'Book price is required'],min:[0,'Price cannot be in negative']},
+    category:{
+        enum:{
+            values:['Fiction' , 'Science' , 'SelfDevelopment' , 'Poetry' , 'Religious'],
+            message:'{VALUE} is not a valid category'
+        },
+        required:[true,'Book category is required']
+    },
+    description:{type:String,required:[true,'Book description is required']},
+    quantity:{type:Number,required:[true,'Book quantity is required']},
+    inStock:{type:Boolean,required:true}
+})
+
+export const ProductModel=model<TProduct>('ProductModel',productSchema)
